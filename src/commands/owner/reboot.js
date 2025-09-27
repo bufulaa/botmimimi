ca424b12f336f125ac44954a950d99bbda9eea72
pam <reidev241@gmail.com>
2025-09-27 09:50:32 +0700

    reboot cmd fix

--- src/commands/owner/reboot.js
+++ src/commands/owner/reboot.js
@@ -1,8 +1,11 @@
 module.exports = {
     name: "reboot",
     run: async (client, message, args) => {
-        if (!process.env.PM2_NAME) message.channel.send({ content: "It seems that you haven't added a PM2_NAME in .env"})
-        message.channel.send({ content: "Okay! I'm rebooting my PM2 \\😃" })
-        eval("pm2 restart " + process.env.PM2_NAME);
+        try {
+            message.channel.send({ content: "Okay! I'm rebooting my PM2 \\😃" })
+            eval("pm2 restart " + process.env.PM2_NAME);
+        } catch {
+            message.channel.send({ content: "It seems that you haven't added a PM2_NAME in .env or you're not running on PM2"})
+        }
     }
 }
